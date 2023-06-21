542189e5670f92148c65a2680a1e18337959b948
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-06-21 15:39:23 +0000

    V1.0.2

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scratchcommunication',
+    version='1.0.2',
+    author='Gine Mosild',
+    author_email='thecommcraft@gmail.com',
+    description='A python module for communicating with scratch projects',
+    long_description='A python module for communicating with scratch projects',
+    long_description_content_type='text/markdown',
+    url='https://github.com/thecommcraft/scratchcommunication',
+    packages=find_packages(exclude=[]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['scratch', 'api'],
+    install_requires=[
+        'requests',
+        'websocket',
+    ],
+    python_requires='>=3.6',
+)
