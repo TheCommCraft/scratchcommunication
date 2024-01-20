2d56d93a3605c9ead156be2ce9c62621ba0494b3
TheCommCraft <simon.c.gilde@gmail.com>
2024-01-20 16:33:47 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scratchcommunication',
-    version='2.0.1',
+    version='2.0.2',
     author='Simon Gilde',
     author_email='simon.c.gilde@gmail.com',
     description='A python module for communicating with scratch projects',
