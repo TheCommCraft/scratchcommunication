77dea673b036113106acacd58904432bf36a9743
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-01-20 16:37:02 +0100

    2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scratchcommunication',
-    version='2.0.2',
+    version='2.0.3',
     author='Gine Mosild',
     author_email='thecommcraft@gmail.com',
     description='A python module for communicating with scratch projects',
