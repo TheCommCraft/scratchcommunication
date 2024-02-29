d1dc64e462317d76e795987460419eb698f42ba0
TheCommCraft <simon.c.gilde@gmail.com>
2024-02-29 18:35:26 +0100

    2.0.7

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name='scratchcommunication',
-    version='2.0.6',
+    version='2.0.7',
     author='Simon Gilde',
     author_email='simon.c.gilde@gmail.com',
     description='A python module for communicating with scratch projects',
