bb93fca6eb4c3e8f056318bdb301c381fcdf6385
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-03-24 20:18:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='scratchcommunication',
-    version='2.2.10',
+    version='2.2.11',
     author='Gine Mosild',
     author_email='thecommcraft@gmail.com',
     description='A python module for communicating with scratch projects',
