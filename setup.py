d5ec521424326c7b4640d9df282bf637874f1865
TheCommCraft <simon.c.gilde@gmail.com>
2024-10-28 19:41:44 +0100

    2.15.3a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
     
-VERSION = '2.15.2'
+VERSION = '2.15.3a'
 
 setup(
     name='scratchcommunication',
