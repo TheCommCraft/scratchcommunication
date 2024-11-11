cb3970d32cf185470b863101418d9c21f25e6bd1
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-11-11 19:56:47 +0100

    2.15.3b

--- scratchcommunication/betas/poly_cloud.py
+++ scratchcommunication/betas/poly_cloud.py
@@ -2,25 +2,24 @@
 Create multiple cloud connections at once
 """
 from scratchcommunication import CloudConnection, TwCloudConnection, Session, Sky
-from typing import Sequence, Mapping, Any
+from typing import Sequence, Mapping, Any, Optional
 
 def scratch_and_turbowarp_connection(
     project_id : int,
     *,
     session : Session,
     as_sky : bool = False,
-    args : Sequence = None,
-    kwargs : Mapping[str, Any] = None
+    args : Optional[Sequence] = None,
+    kwargs : Optional[Mapping[str, Any]] = None
 ) -> tuple[CloudConnection, CloudConnection]:
     """
     Create a turbowarp connection with a normal one.
     """
-    args = args or ()
-    kwargs = kwargs or {}
-    cloud_1 = session.create_cloudconnection(project_id=project_id, *args, **kwargs)
-    cloud_2 = session.create_tw_cloudconnection(project_id=project_id, *args, **kwargs)
-    if as_sky:
-        return Sky(cloud_1, cloud_2)
+    assert not as_sky
+    args = tuple(args or ())
+    kwargs = dict(kwargs or {})
+    cloud_1 = session.create_cloudconnection(project_id, *args, **kwargs)
+    cloud_2 = session.create_tw_cloudconnection(project_id, *args, **kwargs)
     return (cloud_1, cloud_2)
 
 def multiple_scratch_connections(
@@ -28,17 +27,16 @@ def multiple_scratch_connections(
     *,
     session : Session,
     as_sky : bool = False,
-    args : Sequence = None,
-    kwargs : Mapping[str, Any] = None
-) -> tuple[CloudConnection]:
+    args : Optional[Sequence] = None,
+    kwargs : Optional[Mapping[str, Any]] = None
+) -> tuple[CloudConnection, ...]:
     """
     Create many normal connections.
     """
-    args = args or ()
-    kwargs = kwargs or {}
-    clouds = tuple(session.create_cloudconnection(project_id=project_id, *args, **kwargs) for project_id in project_ids)
-    if as_sky:
-        return Sky(*clouds)
+    assert not as_sky
+    args = tuple(args or ())
+    kwargs = dict(kwargs or {})
+    clouds = tuple(session.create_cloudconnection(project_id, *args, **kwargs) for project_id in project_ids)
     return clouds
 
 def multiple_scratch_and_turbowarp_connections(
@@ -46,16 +44,15 @@ def multiple_scratch_and_turbowarp_connections(
     *,
     session : Session,
     as_sky : bool = False,
-    args : Sequence = None,
-    kwargs : Mapping[str, Any] = None
-) -> tuple[CloudConnection]:
+    args : Optional[Sequence] = None,
+    kwargs : Optional[Mapping[str, Any]] = None
+) -> tuple[CloudConnection, ...]:
     """
     Create many normal connections with turbowarp ones.
     """
-    args = args or ()
-    kwargs = kwargs or {}
-    clouds = tuple(session.create_cloudconnection(project_id=project_id, *args, **kwargs) for project_id in project_ids) + \
-        tuple(session.create_tw_cloudconnection(project_id=project_id, *args, **kwargs) for project_id in project_ids)
-    if as_sky:
-        return Sky(*clouds)
+    assert not as_sky
+    args = tuple(args or ())
+    kwargs = dict(kwargs or {})
+    clouds = tuple(session.create_cloudconnection(project_id, *args, **kwargs) for project_id in project_ids) + \
+        tuple(session.create_tw_cloudconnection(project_id, *args, **kwargs) for project_id in project_ids)
     return clouds
