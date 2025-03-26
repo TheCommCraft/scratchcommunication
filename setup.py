a55dfcc4d3c309abbed81806489b376df1e96267
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2025-03-26 12:38:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
     
-VERSION = '2.15.3'
+VERSION = "2.15.3"
 
 setup(
     name='scratchcommunication',
